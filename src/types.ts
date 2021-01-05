fb8fee92422fc5125f7694a3964ffefff7b2b69c
Bruno <brunochirelli@gmail.com>
2021-01-05 16:59:56 -0300

    add types for inputs

--- src/types.ts
+++ src/types.ts
@@ -3,13 +3,22 @@
  */
 export type InvestmentData = {
   investmentName: string;
-  type: string;
-  tax: string;
+  rate: number;
   initialInvestment: number;
   monthInvestment: number;
-  finalValue: number;
+  futureValue: number;
   grossProfit: number;
   irPercent: number;
   irValue: number;
   netProfit: number;
+};
+
+export type InvestmentInputs = {
+  investmentName: string;
+  rate: number;
+  initialInvestment: number;
+  monthInvestment: number;
+  presentValue: number;
+  startDate: string;
+  endDate: string;
 };
