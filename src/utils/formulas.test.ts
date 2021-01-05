2392774affc4c2da24819b349df21adc34ec88e3
Bruno <brunochirelli@gmail.com>
2021-01-05 17:01:44 -0300

    add test for formulas functions

--- src/utils/formulas.test.ts
+++ src/utils/formulas.test.ts
@@ -1,8 +1,26 @@
-import { futureValue } from "./formulas";
+import { futureValue, incomeTaxCalculator } from "./formulas";
 
 describe("FV basic functionality", () => {
-  it("returns the right amount", () => {
-    expect(futureValue(0.06, 10, -200, -500, 1)).toBe(3689.75237605618);
-    expect(futureValue(6 / 100, 10, -200, -500, 1)).toBe(3689.75237605618);
+  it("should returns the right amount", () => {
+    expect(futureValue(0.06, 10, -200, -500)).toBe(3531.58);
+    expect(futureValue(6 / 100, 10, -200, -500)).toBe(3531.58);
+    expect(futureValue(0.06, 0, -100, -500)).toBe(500);
+    expect(futureValue(0.06, 1, -200, -500)).toBe(730);
   });
+});
+
+describe("income tax handler basic functionality", () => {
+  it("should return the right income tax for a given period", () => {
+    expect(incomeTaxCalculator(10)).toBe(0.225);
+    expect(incomeTaxCalculator(180)).toBe(0.225);
+    expect(incomeTaxCalculator(181)).toBe(0.2);
+    expect(incomeTaxCalculator(360)).toBe(0.2);
+    expect(incomeTaxCalculator(361)).toBe(0.175);
+    expect(incomeTaxCalculator(720)).toBe(0.175);
+    expect(incomeTaxCalculator(721)).toBe(0.15);
+  });
+
+  // it("should handle wrong inputs", () => {
+  //   expect(() => incomeTaxCalculator("10")).toThrow();
+  // });
 });
