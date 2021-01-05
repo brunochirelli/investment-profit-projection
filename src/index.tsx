7e7cd4f219c7da214ba0770f97c5624d881817f8
Bruno <brunochirelli@gmail.com>
2021-01-05 17:00:36 -0300

    add provider and baseline styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CssBaseline } from "@material-ui/core";
+import { DataProvider } from "store/DataProvider";
 import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <CssBaseline />
+    <DataProvider>
       <App />
-    </Provider>
+    </DataProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
