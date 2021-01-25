9f6556b1039cc9029f50e2293a312d8b8e8df2d9
Bruno <brunochirelli@gmail.com>
2021-01-25 19:40:19 -0300

    home component to link to calculators

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,47 +1,23 @@
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { Box, Button, Collapse, Container, useTheme } from "@material-ui/core";
-
-import { DataContext } from "store/DataProvider";
-
-import FutureValueResult from "components/FutureValueResult";
-import FutureValueForm from "../components/FutureValueForm";
-import Layout from "components/Layout";
+import { Container, Typography, Box } from "@material-ui/core";
 
 const Home = () => {
-  const [expanded, setExpanded] = useState(true);
-  const context = useContext(DataContext);
-  const { state } = context;
-  const theme = useTheme();
-
-  useEffect(() => {
-    if (state.isCalculated) {
-      setExpanded(false);
-    }
-  }, [state.isCalculated]);
-
   return (
-    <Layout>
-      <Container maxWidth="sm">
-        {expanded ? null : (
-          <Box
-            width="100%"
-            padding={1}
-            marginBottom={2}
-            borderRadius="0 0 0.5rem 0.5rem"
-            bgcolor={theme.palette.secondary.main}
-          >
-            <Button fullWidth onClick={() => setExpanded(true)}>
-              Editar
-            </Button>
-          </Box>
-        )}
-        <Collapse in={expanded}>
-          <FutureValueForm collapseForm={setExpanded} />
-        </Collapse>
-        <FutureValueResult state={state} />
-      </Container>
-    </Layout>
+    <Container component={Box} py={2}>
+      <Typography variant="h3" gutterBottom>
+        Calculadoras de Investimentos
+      </Typography>
+      <Typography
+        variant="h5"
+        component={Link}
+        to="/future-value"
+        style={{ color: "inherit" }}
+      >
+        Future Value
+      </Typography>
+    </Container>
   );
 };
 
